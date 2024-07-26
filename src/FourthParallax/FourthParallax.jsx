7783a8f77d7fea0f1203138c92e58756251cdc64
rohim48 <noerrohim10@gmail.com>
2024-07-27 06:34:44 +0700

    slicing pricing page

--- src/FourthParallax/FourthParallax.jsx
+++ src/FourthParallax/FourthParallax.jsx
@@ -1,30 +1,37 @@
-import './FourthParallax.css';
+import "./FourthParallax.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 function goto(link) {
-    const urls = {
-      chat: "../FourthParallax/FourthParallax.jsx", // untuk ke page 4 (contact)
-    };
-  
-    window.location.href = urls[link];
-  }
+  const urls = {
+    chat: "../FourthParallax/FourthParallax.jsx",
+  };
+
+  window.location.href = urls[link];
+}
+
 const FourthParallax = () => {
   return (
     <div id="fourthParallax" className="fourth parallax">
       <div className="contact">
         <div className="contact-info">
           <h4>Temukan Mobilmu Sekarang</h4>
-          <button className="button-explore" onClick={() => goto('chat')}>
+          <button className="button-explore" onClick={() => goto("chat")}>
             Cari Mobil Bekas
           </button>
           <h2 className="content-title">Hubungi kami di :</h2>
-          <div>
-            <i className="fa-brands fa-facebook" style={{color: "#000000"}}>Mobilmurah ID</i>
-            <i className="fa-brands fa-whatsapp" style={{color: "#000000"}}>+628123456789</i>
-            <p>
-              Call center kami akan melayani anda 24/7 dengan senang hati
-              melayani semua kebutuhan anda mencari mobil yang terbaik.
-            </p>
+          <div className="contact-icons">
+            <div className="contact-item">
+              <FontAwesomeIcon icon={faFacebook} /> Mobilmurah ID
+            </div>
+            <div className="contact-item">
+              <FontAwesomeIcon icon={faWhatsapp} /> +628123456789
+            </div>
           </div>
+          <p>
+            Call center kami akan melayani anda 24/7 dengan senang hati melayani
+            semua kebutuhan anda mencari mobil yang terbaik.
+          </p>
         </div>
       </div>
     </div>
