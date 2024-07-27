67d0b247e3b153dbe1abfccd04849b0e683fd48d
rohim48 <noerrohim10@gmail.com>
2024-07-27 08:13:15 +0700

    fixing footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import './Footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer>
       <p>
-        Created by Noer Rohim, harisenin.com SIM-K FSD Participant &#169; 2024
+        Created by Noer Rohim &#169; 2024
       </p>
       <div className="sosmed">
-        <a href="http://instagram.com/nr.him"><i className="fa-brands fa-instagram"></i></a>
-        <span className="ig">nr.him</span>
-        <a href="https://www.linkedin.com/in/noer-rohim-2020/"><i className="fa-brands fa-linkedin-in"></i></a>
-        <span className="linkedin">Noer Rohim</span>
+        <a href="http://instagram.com/nr.him"><FontAwesomeIcon icon={faInstagram} /> nr.him </a>
+        <a href="https://www.linkedin.com/in/rohim48/"><FontAwesomeIcon icon={faLinkedin} /> Noer Rohim </a>
       </div>
     </footer>
   );
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header/Header.jsx";
-import "./Main.css";
 import Footer from "./Footer/Footer.jsx";
 import FirstParallax from "./FirstParallax/FirstParallax.jsx";
-import FourthParallax from "./FourthParallax/FourthParallax.jsx";
 import SecondParallax from "./SecondParallax/SecondParallax.jsx";
 import ThirdParallax from "./ThirdParallax/ThirdParallax.jsx";
+import FourthParallax from "./FourthParallax/FourthParallax.jsx";
+import "./Main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
